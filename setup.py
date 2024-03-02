42b56a31069fdf7f0e7447787c9704a66ea436f9
Shashi Kumar Soni <skssunny30@gmail.com>
2024-03-02 17:11:43 +0530

    changes on setup

--- setup.py
+++ setup.py
@@ -5,19 +5,15 @@ packages = find_packages(
     exclude=("doc", "dist", "build", "fslpy.egg-info", "tests")
 )
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "requirements.txt"), "rt") as f:
-    install_requires = [l.strip() for l in f.readlines()]
-
 setup(
     name = "flatter",
     version = "0.0.1",
     description = "Retuns the dataframe for the dictionary",
     author = "Shashi Kumar",
     author_email = "skssunny30@gmail.com",
+    long_description_content_type = "text/markdown",
+    long_description= "This is a python package which converts a nested dictionary into pandas dataframe",
     packages = packages,
-    install_requires = install_requires,
     license = "MIT",
     url = "https://github.com/walkershashi/flatter",
 
