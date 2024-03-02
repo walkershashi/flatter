883617af9607c7ccedde67c02ae3d7e1cdf6514c
Shashi Kumar Soni <skssunny30@gmail.com>
2024-03-02 17:30:59 +0530

    adding pandas

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ packages = find_packages(
     exclude=("doc", "dist", "build", "fslpy.egg-info", "tests")
 )
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, "requirements.txt"), "rt") as f:
-    install_requires = [l.strip() for l in f.readlines()]
-
 setup(
     name = "flatter",
     version = "0.0.1",
@@ -19,7 +14,7 @@ setup(
     long_description_content_type = "text/markdown",
     long_description= "This is a python package which converts a nested dictionary into pandas dataframe",
     packages = packages,
-    install_requires = install_requires,
+    install_requires = ["pandas"],
     license = "MIT",
     url = "https://github.com/walkershashi/flatter",
 
